116a1f5381c937730f71deceff092f2fb9702bed
Adam Garberg <adamgarberg@gmail.com>
2022-03-23 20:52:10 -0500

    server side completed for hours data

---
+++ server/routes/hours.router.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+
+router.get('/weekly/:id', (req, res) => {
+  const userId = req.user.id
+  queryText = `SELECT date_trunc('week', date ) as start_of_week, SUM (time_paid) as weekly_hours
+  FROM jobs WHERE "user_id" = 1
+  GROUP BY date_trunc('week', date )
+  ORDER BY date_trunc('week', date ) DESC 
+  LIMIT 5;`
+  queryValue = [userId]
+  pool.query
+  .then((result) => {
+    res.sendStatus(200);
+}).catch((error) => {
+    console.log('Error getting weekly hours', error);
+    res.sendStatus(500);
+    })
+});
+
+router.get('/daily:id', (req, res) => {
+  const userId = req.user.id
+  queryText = `SELECT date_trunc('day', date ) as day, SUM (time_paid) as daily_hours
+  FROM jobs WHERE "user_id" = $1
+  GROUP BY date_trunc('day', date )
+  ORDER BY date_trunc('day', date ) DESC 
+  LIMIT 5;`
+  queryValue = [userId]
+  pool.query
+  .then((result) => {
+    res.sendStatus(200);
+}).catch((error) => {
+    console.log('Error getting daily hours', error);
+    res.sendStatus(500);
+    })
+});
+
+router.get('/monthly:id', (req, res) => {
+  const userId = req.user.id
+  queryText = `SELECT date_trunc('month', date ) as start_of_month, SUM (time_paid) as monthly_hours
+  FROM jobs WHERE "user_id" = $1
+  GROUP BY date_trunc('month', date )
+  ORDER BY date_trunc('month', date ) DESC 
+  LIMIT 6:` 
+  queryValue = [userId]
+  pool.query
+  .then((result) => {
+    res.sendStatus(200);
+}).catch((error) => {
+    console.log('Error getting monthly hours', error);
+    res.sendStatus(500);
+    })
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 const jobRouter = require('./routes/job.router')
+const hoursRouter = require('./routes/hours.router')
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -25,6 +26,7 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use('/api/job', jobRouter);
+app.use('/api/hours', hoursRouter);
 
 // Serve static files
 app.use(express.static('build'));
